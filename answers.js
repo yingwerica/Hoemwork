f4ee54c4964c3641e1a727900d4e477e6686b0aa
yingwerica <yingw.pro@gmail.com>
2022-10-21 12:06:41 -0600

    add HTML & CSS rankings

---
+++ answers.js
@@ -0,0 +1,6 @@
+///////HTML & CSS ////////////
+//HTML Intro (tags, content, attributes, semantic HTML)---Rank: 5;
+//HTML Elements (headers, navigation, lists, sections, etc.) and Media (images, audio, videos)---Rank: 4;
+//CSS Intro (including css files in HTML, elements)---Rank: 5;
+//CSS Basics (colors, background, font properties)---Rank: 4;
+
