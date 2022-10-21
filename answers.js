ba97a62844bc059b38b5c42466940f8fed892079
yingwerica <yingw.pro@gmail.com>
2022-10-21 15:25:34 -0600

    Methods Revisited answered

--- answers.js
+++ answers.js
@@ -75,6 +75,17 @@ console.log(sliceFav);
 console.log(favMovies);
 console.log(favMovies.indexOf("Fast and Furious"));// return 14
 
+//Where is Waldo
+const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+                    ["Lucinda", "Jacc", "Neff", "Snoop"],
+                    ["Petunia", ["Baked Goods", "Waldo"]]];
+whereIsWaldo.splice(1,1);
+console.log(whereIsWaldo);
+whereIsWaldo[1][2] = "No One";
+console.log(whereIsWaldo);
+console.log(whereIsWaldo[2][1][1]);
+
+
 
 
 
